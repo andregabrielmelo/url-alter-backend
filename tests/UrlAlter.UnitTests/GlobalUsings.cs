628101e4966209657ffa37041f62d283849dbc4d
André Melo <andregabrielmelo@gmail.com>
2025-10-03 22:36:21 -0300

    Backend submodule initial commit

---
+++ tests/UrlAlter.UnitTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System.Runtime.CompilerServices;
+global using Ardalis.SharedKernel;
+global using url_alter_backend.Core.ContributorAggregate;
+global using url_alter_backend.UseCases.Contributors.Create;
+global using Shouldly;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using NSubstitute;
+global using Xunit;
